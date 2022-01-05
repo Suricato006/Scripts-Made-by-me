bff5c83673e7136f42eda8fc9d56896441184432
Suricato <alberto.direnzo2@gmail.com>
2022-01-05 18:34:26 +0100

    Changed the discord link

--- CrabHub.lua
+++ CrabHub.lua
@@ -1,9 +1,9 @@
 --[[ 
-    
+
 CrabHub
 Created by Suricato006#8711
 
-if you are someone that wants some script for a game you want to exploit ask in the discord server (https://discord.gg/JSjpgSPs4v), i will make one for sure.
+if you are someone that wants some script for a game you want to exploit ask in the discord server (https://discord.gg/5NYqSVwH9Q), i will make one for sure.
 
 The script is open source so you can learn from it, not steal it. If you want to use my script you are free to do it, just credit me.
 Much love and thanks for using my script <3
