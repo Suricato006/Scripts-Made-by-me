278e39fa8b07c0580dd17fefe9de12ecb8358c04
Suricato <alberto.direnzo2@gmail.com>
2022-02-15 18:55:18 +0100

    Double Exp Fix 2

--- Double Exp Freeze Final Stand.lua
+++ Double Exp Freeze Final Stand.lua
@@ -1,14 +1,16 @@
-local Players = game:GetService("Players")
 if not game:IsLoaded() then game.Loaded:Wait() end
 
 local FinalStandTable = {536102540, 569994010, 882399924, 2046990924, 478132461, 3552157537, 2651456105, 3565304751, 882375367}
 
-if not table.find(FinalStandTable, game.PlaceId) then
-    return
-end
+if not table.find(FinalStandTable, game.PlaceId) then return end
+
+while not game.Players.LocalPlayer.Character do task.wait() end
+
+task.wait(0.5)
 
 game:GetService("RunService").Heartbeat:Connect(function()
-    pcall(function()
-        game.Players.LocalPlayer.Character.True:Destroy()
-    end)
+    local a = game.Players.LocalPlayer.Character:FindFirstChild("True")
+    if a then
+        a:Destroy()
+    end
 end)
