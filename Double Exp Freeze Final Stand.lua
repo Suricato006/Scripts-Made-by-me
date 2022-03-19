30094aaa93d7183435d744d92056d1ccd866b0eb
Suricato <alberto.direnzo2@gmail.com>
2022-03-19 16:05:50 +0100

    Exp freeze

--- Double Exp Freeze Final Stand.lua
+++ Double Exp Freeze Final Stand.lua
@@ -1,9 +1,4 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-
-local FinalStandTable = {536102540, 569994010, 882399924, 2046990924, 478132461, 3552157537, 2651456105, 3565304751, 882375367, 3552158750, 535527772}
-
-if not table.find(FinalStandTable, game.PlaceId) then return end
-
 local Player = game.Players.LocalPlayer
 
 while not Player.Character do task.wait() end
