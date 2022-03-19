e90c7eaa394118391c97edd33771e5724c0981fb
Suricato <alberto.direnzo2@gmail.com>
2022-03-19 12:31:18 +0100

    Fixing stuff

--- Final Stand AutoFarm.lua
+++ Final Stand AutoFarm.lua
@@ -1,5 +1,4 @@
 local Player = game.Players.LocalPlayer
-local RunService = game:GetService("RunService")
 
 _G.NpcName = ""
 _G.AutoFarm = false
@@ -17,7 +16,6 @@ local function Pugno()
 end
 
 local Npc = nil
-local Tweening = false
 
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/GX28T0pH"))()
 local Main = Library:CreateWindow("AutoFarm Final Stand")
@@ -30,24 +28,17 @@ Main:AddToggle({text = "AutoFarm", state = false, callback = function(bool)
                 Npc = FindNpc()
             end
             if Npc then
-                if not string.find(Npc.Name, _G.NpcName) then
-                    Npc = nil
-                    return
-                end
                 if not (Npc.Humanoid.Health > 0) then
                     Npc = FindNpc()
-                    return
                 end
-                if not Tweening then
+                if Npc then
                     if ((HRP.Position - Npc.HumanoidRootPart.Position).magnitude <= 900) then
                         HRP.CFrame = CFrame.new(Npc.HumanoidRootPart.Position - Npc.HumanoidRootPart.CFrame.LookVector/2, Npc.HumanoidRootPart.Position)
                         Pugno()
                     else
                         local tween = game:GetService("TweenService"):Create(HRP, TweenInfo.new(3), {CFrame = CFrame.new(Npc.HumanoidRootPart.Position - Npc.HumanoidRootPart.CFrame.LookVector/2, Npc.HumanoidRootPart.Position)})
                         tween:Play()
-                        Tweening = true
                         tween.Completed:Wait()
-                        Tweening = false
                     end
                 end
             end
