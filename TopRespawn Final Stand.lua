9a7a79fdc020d0dadc1eabd8fba28e6cb2348765
Suricato <alberto.direnzo2@gmail.com>
2022-03-30 14:16:50 +0200

    top respawn

--- TopRespawn Final Stand.lua
+++ TopRespawn Final Stand.lua
@@ -1,8 +1,22 @@
 local Player = game.Players.LocalPlayer
 _G.TopRespawn = true
-while _G.TopRespawn do task.wait()
-    local SuperAction = Player.Character:FindFirstChild("SuperAction")
-    if SuperAction then
-        SuperAction:Destroy()
+coroutine.wrap(function()
+    while _G.TopRespawn do task.wait()
+        local SuperAction = Player.Character:FindFirstChild("SuperAction")
+        if SuperAction then
+            SuperAction:Destroy()
+        end
+    end
+end)
+while _G.TopRespawn do
+    local Humanoid = Player.Character:FindFirstChild("Humanoid")
+    local HRP = Player.Character:FindFirstChild("HumanoidRootPart")
+    if Humanoid.Health <= 1 then
+        repeat
+            task.wait()
+        until Humanoid.Health == Humanoid.MaxHealth
+        local tween = game:GetService("TweenService"):Create(HRP,TweenInfo.new(1,  Enum.EasingStyle.Quad),{CFrame = CFrame.new(100, 100, 100)})
+        tween:Play()
+        tween.Completed:Wait()
     end
 end
