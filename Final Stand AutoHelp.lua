ecc24cf6f096ba1014b3158cbea7c0315c6c6f48
Suricato <alberto.direnzo2@gmail.com>
2022-03-30 18:15:19 +0200

    Autohelp

---
+++ Final Stand AutoHelp.lua
@@ -0,0 +1,22 @@
+_G.Help = true
+_G.PlayerToHelp = "suricato006"
+
+
+local Player = game.Players.LocalPlayer
+local HelpedPlayer = game.Players:FindFirstChild(_G.PlayerToHelp)
+if not HelpedPlayer then return end
+while _G.Help do task.wait()
+    local Hrp = Player.Character:FindFirstChild("HumanoidRootPart")
+    local HelpHrp = HelpedPlayer.Character:FindFirstChild("HumanoidRootPart")
+    local PowerOutput = Player.Character:FindFirstChild("PowerOutput")
+    if PowerOutput then
+        PowerOutput:Destroy()
+    end
+    if Hrp and HelpHrp then
+        Hrp.CFrame = HelpHrp.CFrame * CFrame.new(0, 20, 0)
+    end
+    local SenzuEvent = Player.Backpack:FindFirstChild("EatSenzu", true)
+    if SenzuEvent then
+        SenzuEvent:FireServer(true)
+    end
+end
