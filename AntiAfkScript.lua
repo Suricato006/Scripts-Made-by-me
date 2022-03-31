7411a9ef4a205f85777516b3de6300b746599174
Suricato <alberto.direnzo2@gmail.com>
2022-03-31 22:16:16 +0200

    update

--- AntiAfkScript.lua
+++ AntiAfkScript.lua
@@ -1,4 +1,14 @@
+local OldNameCall = nil
 local Player = game.Players.LocalPlayer
+OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
+    local NamecallMethod = getnamecallmethod()
+    if (not checkcaller()) and (Self == Player) and (NamecallMethod == "Kick") then
+        return nil
+    end
+
+    return OldNameCall(Self, ...)
+end)
+
 local VirtualUser = game:GetService("VirtualUser")
 Player.Idled:Connect(function()
     VirtualUser:CaptureController()
