64fe77ad81862bcc23143f4fe77dd0720589eef9
Suricato <alberto.direnzo2@gmail.com>
2022-06-26 14:52:32 +0200

    fix

--- Scripts/RandomScripts/Anime Fighters/AutoFarm.lua
+++ Scripts/RandomScripts/Anime Fighters/AutoFarm.lua
@@ -3,19 +3,7 @@ _G.AutoCollect = not _G.AutoCollect
 
 local Player = game.Players.LocalPlayer
 
-local Collectables = {"Yen", "GenericItem"}
-local Connection = nil
-Connection = game:GetService("Workspace").Effects.ChildAdded:Connect(function(Child)
-    if not _G.AutoCollect then
-        Connection:Disconnect()
-        return
-    end
-    if table.find(Collectables, Child.Name) or string.find(Child.Name, "Shard") then
-        while true do task.wait()
-            Child:WaitForChild("Base").CFrame = Player.Character.HumanoidRootPart.CFrame
-        end
-    end
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Suricato006/Scripts-Made-by-me/master/Scripts/RandomScripts/Anime%20Fighters/AutoClick%20and%20Collect.lua"))()
 
 while _G.AutoFarm do task.wait()
     for i, v in pairs(workspace.Worlds[Player.World.Value].Enemies:GetChildren()) do
@@ -23,7 +11,6 @@ while _G.AutoFarm do task.wait()
         game:GetService("ReplicatedStorage").Bindable.SendPet:Fire(v, true, true)
         while _G.AutoFarm and (v:FindFirstAncestor("Worlds")) do task.wait()
             Player.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
-            game:GetService("ReplicatedStorage").Remote.ClickerDamage:FireServer()
         end
     end
 end
--- Scripts/RandomScripts/Anime Fighters/AutoTimeTrial.lua
+++ Scripts/RandomScripts/Anime Fighters/AutoTimeTrial.lua
@@ -1,30 +1,9 @@
 local Player = game.Players.LocalPlayer
 local InputLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Suricato006/Scripts-Made-by-me/master/Libraries/InputFunctions%20Library.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Suricato006/Scripts-Made-by-me/master/Scripts/RandomScripts/Anime%20Fighters/AutoClick%20and%20Collect.lua"))()
+
 _G.AutoTimeTrial = not _G.AutoTimeTrial
-
-local Collectables = {"Yen", "GenericItem"}
-game:GetService("Workspace").Effects.ChildAdded:Connect(function(Child)
-    if table.find(Collectables, Child.Name) or string.find(Child.Name, "Shard") then
-        while _G.AutoTimeTrial do task.wait()
-            Child:WaitForChild("Base").CFrame = Player.Character.HumanoidRootPart.CFrame
-        end
-    end
-end)
-
-local Optimized = false
-if getconnections then
-    local Connection = Player.PlayerGui.MainGui.ClickFrame.Click.Activated
-    repeat
-        task.wait()
-    until getconnections(Connection)[2]
-    Optimized = true
-    coroutine.wrap(function()
-        while _G.AutoTimeTrial do task.wait()
-            getconnections(Connection)[1]:Fire()
-        end
-    end)()
-end
 
 while _G.AutoTimeTrial do task.wait()
     for i, v in pairs(workspace.Worlds[Player.World.Value].Map:GetChildren()) do
@@ -43,9 +22,6 @@ while _G.AutoTimeTrial do task.wait()
         game:GetService("ReplicatedStorage").Bindable.SendPet:Fire(v, true, true)
         while v:FindFirstAncestor("Worlds") and _G.AutoTimeTrial do task.wait()
             Player.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
-            if not Optimized then
-                game:GetService("ReplicatedStorage").Remote.ClickerDamage:FireServer()
-            end
         end
     end
 end
